1c69fe15ae6b51fc96c455bd051eb3d1db2bb0ab
Priya Bhoomireddi <priyagetlucky1011@gmail.com>
2024-07-12 15:00:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function stringChop(str, size) {
-  // your code here
+  // your code hereif (!str) {
+    return [];
+  }
+
+  const chunks = [];
+  let index = 0;
+
+  while (index < str.length) {
+    chunks.push(str.substring(index, index + size));
+    index += size;
+  }
+
+  return chunks;
 }
 
 // Do not change the code below
